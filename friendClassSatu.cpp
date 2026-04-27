b604223710bb0f804f7691d0ecf13af7f51be191
fzfrmdni-dev <fzfrmdni@gmail.com>
2026-04-27 18:27:37 +0700

    siswa

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -12,6 +12,7 @@ public:
     void setNama(string pNama);
     friend class siswa;
 };
+
 class siswa
 {
 private:
@@ -21,3 +22,16 @@ public:
     void setId(int pId);
     void displayAll(orang &a);
 };
+
+void siswa::displayAll(orang &a)
+{       
+     cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
